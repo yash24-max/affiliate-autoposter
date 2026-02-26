890127fcaa26f08da08f9d9ae6c5f9c3cf067663
Yasvanthkumar <yasvanth.kumar@kapturecrm.com>
2026-02-26 17:06:16 +0530

    init

---
+++ autoposter-backend/eureka-service/src/test/java/com/autoposter/eureka/EurekaServerApplicationTest.java
@@ -0,0 +1,12 @@
+package com.autoposter.eureka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTest {
+
+    @org.junit.jupiter.api.Test
+    void contextLoads() {
+    }
+
+}
